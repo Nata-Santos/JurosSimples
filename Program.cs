6f4fe617c1e1067bf181b0de4fe6b6f69312578e
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-26 22:19:59 -0300

    entrega Juros simples

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal c, i , t;
+            decimal juros, mont;
+            
+            Console.WriteLine("Juros Simples .");
+            Console.WriteLine();
+            
+            Console.Write("Capital (R$).......:");
+            c = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Taxa de Juros (%)..:");
+            i = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Tempo (meses).....:");
+            t = Convert.ToDecimal(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.WriteLine("---------------------");
+
+            juros = c * i * t *1 /100 ;
+            mont = c + juros;
+            Console.WriteLine($"\nJuros (R$)....: {juros:N2}");
+            Console.WriteLine($"Montante(R$)..: {mont:N2}");
+            }
+    }
+}
